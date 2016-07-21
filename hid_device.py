43f0787740e2248de8cf4306c4319a60e797598e
nbness2 <nbness1337@gmail.com>
2016-07-21 03:08:45 -0600

    [ADD] press(), hotkey()
    added these for future when I want controller to do things.
    
    other stuff some reason forgot to put in other commits oops.

--- hid_device.py
+++ hid_device.py
@@ -1,4 +1,5 @@
 from pywinusb import hid
+from pyautogui import press, hotkey
 from time import sleep
 
 
@@ -6,8 +7,6 @@ hids = hid.find_all_hid_devices()
 with open('curr_hid.txt', 'r') as curr_hid:
     current_hid = curr_hid.readline().split(',')
 curr_device = hids[int(current_hid[0])-1]
-
-buttonmaps = {'0xbead': {'port': 0, 'lsx': 2, 'lsy': 6, 'lt': 10, 'csx': 14, 'csy': 18, 'rt': 22, 'buttons': (49, True), 'dpad': (50, True)}}
 
 
 class Axis:
@@ -61,6 +60,8 @@ class Buttons:
 buttonmaps = {'0xbead': {'lsx': Axis(2, 58, 68), 'lsy': Axis(6, 58, 67), 'lt': Axis(10, 10, 105), 'rt': Axis(22, 10, 105),
                          'csx': Axis(14, 59, 67), 'csy': Axis(18, 59, 68), 'buttons': Buttons(49, 'abxyzrl'), 'dpad': Buttons(50, 'udlr')}}
 
+gcbuttons = ('lsx', 'lsy', 'lt', 'rt', 'csx', 'csy', 'buttons', 'dpad')
+
 
 def handle(rawdata, xargs=(None, None)):
     data = rawdata
@@ -90,6 +91,8 @@ def device_test(device, buttons=None, delay=1):
             print('device is detected')
             device.open()
             print('device opened successfully')
+            print('recieving input after {} seconds.'.format(delay))
+            sleep(delay)
             device.set_raw_data_handler(handle, (device.product_id, buttons))
             while device.is_plugged():
                 sleep(1)
